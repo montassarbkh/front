488165aa2c066ae21fa78f327a57882f110df402
montassar <2130091@isi-montreal.com>
2023-04-26 12:09:20 -0400

    front

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './component/Header'
+import Copyright from './component/Copyright'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Copyright/>
+      
+        
+    </>
+  )
+}
+
+export default App
